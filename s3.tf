fe1aa1c744d7ece3c47817dbc615ce08926806ee
YUKESH R <143831307+YuksR@users.noreply.github.com>
2024-05-25 22:28:08 +0530

    Add files via upload

---
+++ s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "yuks-terraform-bucket"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
